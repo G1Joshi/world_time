ca22b16316e517f641b863907c060665f306258f
G1Joshi <joshijeevan98@gmail.com>
2021-03-10 21:41:05 +0530

    add worldtime services

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/services/worldtime.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -6,16 +7,32 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  String time = 'loading...';
+  void setWorldTime() async {
+    WorldTime instance = WorldTime(
+        url: 'Asia/Kolkata', location: 'Kolkata', flag: 'Kolkata.png');
+    await instance.getTime();
+    setState(() {
+      time = instance.time;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setWorldTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey[800],
       appBar: AppBar(
-      backgroundColor: Colors.blueGrey[900],
+        backgroundColor: Colors.blueGrey[900],
         title: Text('Loading'),
       ),
       body: SafeArea(
-        child: Text('Loading'),
+        child: Text(time),
       ),
     );
   }
---
+++ lib/services/worldtime.dart
@@ -0,0 +1,26 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+  String url;
+  String location;
+  String flag;
+  String time = 'loading...';
+
+  WorldTime({required this.url, required this.location, required this.flag});
+
+  Future<void> getTime() async {
+    var api = Uri.https(
+        'www.worldtimeapi.org', '/api/timezone/$url', {'q': '{https}'});
+    Response response = await get(api);
+    Map data = jsonDecode(response.body);
+    String datetime = data['datetime'];
+    String hours = data['utc_offset'].substring(1, 3);
+    String minutes = data['utc_offset'].substring(4, 6);
+    
+    DateTime now = DateTime.parse(datetime)
+        .add(Duration(hours: int.parse(hours)))
+        .add(Duration(minutes: int.parse(minutes)));
+    time = now.toString();
+  }
+}

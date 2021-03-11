760e98085b35dcaff4879ad860262e69a63bdde2
G1Joshi <joshijeevan98@gmail.com>
2021-03-11 22:22:56 +0530

    add loading widget

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,8 +6,11 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  dynamic data = {};
+
   @override
   Widget build(BuildContext context) {
+    data = ModalRoute.of(context)!.settings.arguments;
     return Scaffold(
       backgroundColor: Colors.blueGrey[800],
       appBar: AppBar(
@@ -15,16 +18,46 @@ class _HomeState extends State<Home> {
         title: Text('Home'),
       ),
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            ElevatedButton.icon(
-              onPressed: () {
-                Navigator.pushNamed(context, '/location');
-              },
-              icon: Icon(Icons.edit_location),
-              label: Text('Edit Location'),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+          child: Column(
+            children: <Widget>[
+              TextButton.icon(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/location');
+                },
+                icon: Icon(Icons.edit_location),
+                label: Text('Edit Location'),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    data['location'],
+                    style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontSize: 28.0,
+                      letterSpacing: 2.0,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                data['time'],
+                style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontSize: 66.0,
+                  letterSpacing: 2.0,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

71e5e1bf15f70b158859ae980a3c1a6da61b37ac
G1Joshi <joshijeevan98@gmail.com>
2021-03-13 14:58:13 +0530

    add countries

--- lib/pages/location.dart
+++ lib/pages/location.dart
@@ -8,20 +8,47 @@ class Location extends StatefulWidget {
 
 class _LocationState extends State<Location> {
   List<WorldTime> locations = [
-    WorldTime(url: 'Asia/kolkata', location: 'India', flag: 'india'),
-    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk'),
-    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa'),
+    WorldTime(url: 'Etc/GMT+2', location: 'africa'),
+    WorldTime(url: 'Etc/GMT-3', location: 'argentina'),
+    WorldTime(url: 'Etc/GMT+11', location: 'australia'),
+    WorldTime(url: 'Etc/GMT+6', location: 'bangladesh'),
+    WorldTime(url: 'Etc/GMT-3', location: 'brazil'),
+    WorldTime(url: 'Etc/GMT-5', location: 'canada'),
+    WorldTime(url: 'Etc/GMT+8', location: 'china'),
+    WorldTime(url: 'Etc/GMT+1', location: 'denmark'),
+    WorldTime(url: 'Etc/GMT+2', location: 'finland'),
+    WorldTime(url: 'Etc/GMT+1', location: 'france'),
+    WorldTime(url: 'Etc/GMT+1', location: 'germany'),
+    WorldTime(url: 'Etc/GMT+2', location: 'greece'),
+    WorldTime(url: 'Asia/kolkata', location: 'india'),
+    WorldTime(url: 'Etc/GMT', location: 'ireland'),
+    WorldTime(url: 'Etc/GMT+2', location: 'israel'),
+    WorldTime(url: 'Etc/GMT+1', location: 'italy'),
+    WorldTime(url: 'Etc/GMT+9', location: 'korea'),
+    WorldTime(url: 'Etc/GMT+1', location: 'netherlands'),
+    WorldTime(url: 'Etc/GMT+1', location: 'norway'),
+    WorldTime(url: 'Etc/GMT+1', location: 'poland'),
+    WorldTime(url: 'Etc/GMT-1', location: 'portugal'),
+    WorldTime(url: 'Etc/GMT+3', location: 'russia'),
+    WorldTime(url: 'Etc/GMT+1', location: 'sweden'),
+    WorldTime(url: 'Etc/GMT+1', location: 'switzerland'),
+    WorldTime(url: 'Etc/GMT+3', location: 'turkey'),
+    WorldTime(url: 'Etc/GMT', location: 'uk'),
+    WorldTime(url: 'Etc/GMT-5', location: 'usa'),
   ];
 
   void updateTime(index) async {
     WorldTime instance = locations[index];
     await instance.getTime();
-    Navigator.pop(context, {
-      'location': instance.location,
-      'time': instance.time,
-      'flag': instance.flag,
-      'day': instance.day,
-    });
+
+    Navigator.pop(
+      context,
+      {
+        'location': instance.location,
+        'time': instance.time,
+        'day': instance.day,
+      },
+    );
   }
 
   @override
@@ -35,25 +62,26 @@ class _LocationState extends State<Location> {
       ),
       body: SafeArea(
         child: ListView.builder(
-            itemCount: locations.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
-                child: Card(
-                  child: ListTile(
-                    onTap: () {
-                      updateTime(index);
-                    },
-                    title: Text(locations[index].location),
-                    leading: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                          'https://raw.githubusercontent.com/G1Joshi/Assets/main/Flags/${locations[index].flag}.png'),
-                    ),
+          itemCount: locations.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+              child: Card(
+                child: ListTile(
+                  onTap: () {
+                    updateTime(index);
+                  },
+                  title: Text(locations[index].location.toUpperCase()),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'https://raw.githubusercontent.com/G1Joshi/Assets/main/Flags/${locations[index].location}.png'),
                   ),
                 ),
-              );
-            }),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
